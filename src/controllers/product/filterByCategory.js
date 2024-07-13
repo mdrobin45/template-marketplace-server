6683209b033bbd712b135a76907ae999af5f65d6
Robin Rana <robin4500bd@gmail.com>
2024-07-13 21:11:36 +0600

    add category filter

--- src/controllers/product/filterByCategory.js
+++ src/controllers/product/filterByCategory.js
@@ -3,8 +3,14 @@ const Product = require("../../schema/product");
 const filterByCategory = async (req, res) => {
    try {
       const { category } = req.query;
-      const result = await Product.find({ category: category });
-      res.send(result);
+
+      if (category === "all") {
+         const result = await Product.find({});
+         res.send(result);
+      } else {
+         const result = await Product.find({ category: category });
+         res.send(result);
+      }
    } catch {
       res.status(500).json({ error: "Internal Server Error" });
    }
